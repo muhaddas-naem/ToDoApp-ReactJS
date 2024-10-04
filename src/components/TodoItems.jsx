41447fa196898fdf777af09950d2a643092c952a
Muhaddas Naem <muhaddasnaem@gmail.com>
2024-10-05 01:00:25 +0600

    ready-todo-app

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import tick from '../assets/tick.png'
+import not_tick from '../assets/not_tick.png'
+import deleteicon from '../assets/delete.png'
+
+
+
+const TodoItems = ({text , id, isComplete, deleteTodo, toogle}) => {
+  return (
+    <div className='flex items-center my-3 gap-2'>
+      <div onClick={()=> {toogle(id)}} className='flex flex-1 items-center cursor-pointer'>
+        <img className='w-7' src={isComplete ? tick : not_tick} alt="" />
+        <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through" : ""}`}>{text}</p>
+      </div>
+
+
+<img onClick={()=>{deleteTodo(id)}} className='w-3.5 cursor-pointer' src={deleteicon} alt="" />
+
+    </div>
+  )
+}
+
+export default TodoItems
